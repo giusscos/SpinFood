176f2929a48343282e672d56e7aad8b34a85c00d
giusscos <giu.cosenza08@gmail.com>
2024-12-21 20:04:19 +0100

    Add suggestions row and example data

---
+++ SpinFood/View/Suggestion/SuggestionRowView.swift
@@ -0,0 +1,18 @@
+//
+//  SuggestionRowView.swift
+//  SpinFood
+//
+//  Created by Giuseppe Cosenza on 21/12/24.
+//
+
+import SwiftUI
+
+struct SuggestionRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuggestionRowView()
+}
---
+++ SpinFood/View/Suggestion/SuggestionsView.swift
@@ -0,0 +1,18 @@
+//
+//  SuggestionsView.swift
+//  SpinFood
+//
+//  Created by Giuseppe Cosenza on 10/12/24.
+//
+
+import SwiftUI
+
+struct SuggestionsView: View {
+    var body: some View {
+        Text("Discover View")
+    }
+}
+
+#Preview {
+    SuggestionsView()
+}
